ab5818249d2587e91618c04b1ffe6f4258a7108d
ChinhHo <tuyetchinh1996@mail.com>
2019-04-11 23:25:52 +0700

    Screen WellCome

--- client/src/main.js
+++ client/src/main.js
@@ -1,12 +1,46 @@
 import Vue from "vue";
 import App from "./App.vue";
-import router from "./router";
 import store from "./store";
-
+import router from "./routes";
+// import Axios from "axios";
 Vue.config.productionTip = false;
-
+// Vue.prototype.$http = Axios;
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+    router,
+    store,
+    render: h => h(App)
 }).$mount("#app");
+
+
+
+/********************* CUSTOM LIBRARY DIRECTIVE ************************/
+Vue.directive("click-outside", {
+    bind: function(el, binding, vNode) {
+        // Provided expression must evaluate to a function.
+        if (typeof binding.value !== "function") {
+            const compName = vNode.context.name;
+            let warn = `[Vue-click-outside:] provided expression '${
+        binding.expression
+      }' is not a function, but has to be`;
+            if (compName) {
+                warn += `Found in component '${compName}'`;
+            }
+
+            console.warn(warn);
+        }
+        const bubble = binding.modifiers.bubble;
+        const handler = e => {
+            if (bubble || (!el.contains(e.target) && el !== e.target)) {
+                binding.value(e);
+            }
+        };
+        el.__vueClickOutside__ = handler;
+
+        document.addEventListener("click", handler);
+    },
+
+    unbind: function(el) {
+        document.removeEventListener("click", el.__vueClickOutside__);
+        el.__vueClickOutside__ = null;
+    }
+});
---
+++ client/src/routes/modules/customer/wellcome.js
@@ -0,0 +1,8 @@
+/** When your routing table is too long, you can split it into small modules**/
+const wellcomeRouter = {
+    path: "/",
+    component: () =>
+        import ("@/views/customer/wellcome")
+};
+
+export default wellcomeRouter;
