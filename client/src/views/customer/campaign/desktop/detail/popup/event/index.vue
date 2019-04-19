f37c4c6773a65b9b6eeb9a80b08e0784ff8af524
Yến Đặng <dangthiyen103@gmail.com>
2019-04-19 11:52:22 +0700

    [Fixed Lint] Campaign: Event popup change color

--- client/src/views/customer/campaign/desktop/detail/popup/event/index.vue
+++ client/src/views/customer/campaign/desktop/detail/popup/event/index.vue
@@ -3,8 +3,9 @@
     <div class="modal--dialog d_flex justify_content_center">
       <div class="modal--content modal--event p_0">
         <!-- Start: Modal Header -->
-        <div class="modal--header modal--event-header p_3"
-          :style="{backgroundColor: activeColor.code}"
+        <div
+          class="modal--header modal--event-header p_3"
+          :style="{ backgroundColor: activeColor.code }"
         >
           <div class="r header--row mx_0">
             <div class="header--row-left mr_3">
@@ -53,7 +54,10 @@
                     :value="true"
                     :sync="true"
                     :color="{ checked: '#FFFFFF', unchecked: '#FFFFFF' }"
-                    :switch-color="{ checked: activeColor.code, unchecked: '#e4e4e4' }"
+                    :switch-color="{
+                      checked: activeColor.code,
+                      unchecked: '#e4e4e4'
+                    }"
                   />
                   Đăng ngẫu nhiên bài viết trên trang cá nhân vào các khung giờ
                   vàng.
@@ -62,29 +66,33 @@
             </div>
             <div class="header--row-right">
               <div class="event--change-color">
-                <div class="label--change"
-                  @click="isShowColorDropdown = true"
-                >Thay đổi màu sắc</div>
+                <div class="label--change" @click="isShowColorDropdown = true">
+                  Thay đổi màu sắc
+                </div>
                 <!-- Start: Change color dropdown -->
                 <div class="dropdown--menu" v-if="isShowColorDropdown">
                   <div class="r mx_0">
-                    <div class="dropdown--menu-item" 
-                      v-for="(i, index) in 3" 
+                    <div
+                      class="dropdown--menu-item"
+                      v-for="(i, index) in 3"
                       :key="index"
                     >
-                      <div class="color--box" 
-                        :style="{backgroundColor: colors[index].code}"
+                      <div
+                        class="color--box"
+                        :style="{ backgroundColor: colors[index].code }"
                         @click="changeColor(colors[index])"
                       ></div>
                     </div>
                   </div>
                   <div class="r mx_0">
-                    <div class="dropdown--menu-item" 
-                      v-for="(i, index) in 3" 
+                    <div
+                      class="dropdown--menu-item"
+                      v-for="(i, index) in 3"
                       :key="index"
                     >
-                      <div class="color--box" 
-                        :style="{backgroundColor: colors[index + 3].code}"
+                      <div
+                        class="color--box"
+                        :style="{ backgroundColor: colors[index + 3].code }"
                         @click="changeColor(colors[index + 3])"
                       ></div>
                     </div>
@@ -106,70 +114,73 @@
               exercitationem dolore error voluptates? Lorem ipsum dolor sit amet
               consectetur adipisicing elit. Quaerat, impedit eos. Voluptatibus
               tempore voluptatum repellat eos quasi doloribus consequatur amet
-              possimus eaque rem. Explicabo, et quod vero dolor dolore voluptates.
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
-              error enim consequuntur eaque, quia excepturi beatae tempora
-              deserunt delectus, quibusdam hic maxime labore. In aut quaerat
+              possimus eaque rem. Explicabo, et quod vero dolor dolore
+              voluptates. Lorem ipsum, dolor sit amet consectetur adipisicing
+              elit. Expedita error enim consequuntur eaque, quia excepturi
+              beatae tempora deserunt delectus, quibusdam hic maxime labore. In
+              aut quaerat exercitationem dolore error voluptates? Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Quaerat, impedit eos.
+              Voluptatibus tempore voluptatum repellat eos quasi doloribus
+              consequatur amet possimus eaque rem. Explicabo, et quod vero dolor
+              dolore voluptates. Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Expedita error enim consequuntur eaque, quia
+              excepturi beatae tempora deserunt delectus, quibusdam hic maxime
+              labore. In aut quaerat exercitationem dolore error voluptates?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+              impedit eos. Voluptatibus tempore voluptatum repellat eos quasi
+              doloribus consequatur amet possimus eaque rem. Explicabo, et quod
+              vero dolor dolore voluptates. Lorem ipsum, dolor sit amet
+              consectetur adipisicing elit. Expedita error enim consequuntur
+              eaque, quia excepturi beatae tempora deserunt delectus, quibusdam
+              hic maxime labore. In aut quaerat exercitationem dolore error
+              voluptates? Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Quaerat, impedit eos. Voluptatibus tempore voluptatum
+              repellat eos quasi doloribus consequatur amet possimus eaque rem.
+              Explicabo, et quod vero dolor dolore voluptates. Lorem ipsum,
+              dolor sit amet consectetur adipisicing elit. Expedita error enim
+              consequuntur eaque, quia excepturi beatae tempora deserunt
+              delectus, quibusdam hic maxime labore. In aut quaerat
               exercitationem dolore error voluptates? Lorem ipsum dolor sit amet
               consectetur adipisicing elit. Quaerat, impedit eos. Voluptatibus
               tempore voluptatum repellat eos quasi doloribus consequatur amet
-              possimus eaque rem. Explicabo, et quod vero dolor dolore voluptates.
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
-              error enim consequuntur eaque, quia excepturi beatae tempora
-              deserunt delectus, quibusdam hic maxime labore. In aut quaerat
+              possimus eaque rem. Explicabo, et quod vero dolor dolore
+              voluptates. Lorem ipsum, dolor sit amet consectetur adipisicing
+              elit. Expedita error enim consequuntur eaque, quia excepturi
+              beatae tempora deserunt delectus, quibusdam hic maxime labore. In
+              aut quaerat exercitationem dolore error voluptates? Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Quaerat, impedit eos.
+              Voluptatibus tempore voluptatum repellat eos quasi doloribus
+              consequatur amet possimus eaque rem. Explicabo, et quod vero dolor
+              dolore voluptates. Lorem ipsum, dolor sit amet consectetur
+              adipisicing elit. Expedita error enim consequuntur eaque, quia
+              excepturi beatae tempora deserunt delectus, quibusdam hic maxime
+              labore. In aut quaerat exercitationem dolore error voluptates?
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat,
+              impedit eos. Voluptatibus tempore voluptatum repellat eos quasi
+              doloribus consequatur amet possimus eaque rem. Explicabo, et quod
+              vero dolor dolore voluptates. Lorem ipsum, dolor sit amet
+              consectetur adipisicing elit. Expedita error enim consequuntur
+              eaque, quia excepturi beatae tempora deserunt delectus, quibusdam
+              hic maxime labore. In aut quaerat exercitationem dolore error
+              voluptates? Lorem ipsum dolor sit amet consectetur adipisicing
+              elit. Quaerat, impedit eos. Voluptatibus tempore voluptatum
+              repellat eos quasi doloribus consequatur amet possimus eaque rem.
+              Explicabo, et quod vero dolor dolore voluptates. Lorem ipsum,
+              dolor sit amet consectetur adipisicing elit. Expedita error enim
+              consequuntur eaque, quia excepturi beatae tempora deserunt
+              delectus, quibusdam hic maxime labore. In aut quaerat
               exercitationem dolore error voluptates? Lorem ipsum dolor sit amet
               consectetur adipisicing elit. Quaerat, impedit eos. Voluptatibus
               tempore voluptatum repellat eos quasi doloribus consequatur amet
-              possimus eaque rem. Explicabo, et quod vero dolor dolore voluptates.
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
-              error enim consequuntur eaque, quia excepturi beatae tempora
-              deserunt delectus, quibusdam hic maxime labore. In aut quaerat
-              exercitationem dolore error voluptates? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Quaerat, impedit eos. Voluptatibus
-              tempore voluptatum repellat eos quasi doloribus consequatur amet
-              possimus eaque rem. Explicabo, et quod vero dolor dolore voluptates.
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
-              error enim consequuntur eaque, quia excepturi beatae tempora
-              deserunt delectus, quibusdam hic maxime labore. In aut quaerat
-              exercitationem dolore error voluptates? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Quaerat, impedit eos. Voluptatibus
-              tempore voluptatum repellat eos quasi doloribus consequatur amet
-              possimus eaque rem. Explicabo, et quod vero dolor dolore voluptates.
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
-              error enim consequuntur eaque, quia excepturi beatae tempora
-              deserunt delectus, quibusdam hic maxime labore. In aut quaerat
-              exercitationem dolore error voluptates? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Quaerat, impedit eos. Voluptatibus
-              tempore voluptatum repellat eos quasi doloribus consequatur amet
-              possimus eaque rem. Explicabo, et quod vero dolor dolore voluptates.
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
-              error enim consequuntur eaque, quia excepturi beatae tempora
-              deserunt delectus, quibusdam hic maxime labore. In aut quaerat
-              exercitationem dolore error voluptates? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Quaerat, impedit eos. Voluptatibus
-              tempore voluptatum repellat eos quasi doloribus consequatur amet
-              possimus eaque rem. Explicabo, et quod vero dolor dolore voluptates.
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
-              error enim consequuntur eaque, quia excepturi beatae tempora
-              deserunt delectus, quibusdam hic maxime labore. In aut quaerat
-              exercitationem dolore error voluptates? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Quaerat, impedit eos. Voluptatibus
-              tempore voluptatum repellat eos quasi doloribus consequatur amet
-              possimus eaque rem. Explicabo, et quod vero dolor dolore voluptates.
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
-              error enim consequuntur eaque, quia excepturi beatae tempora
-              deserunt delectus, quibusdam hic maxime labore. In aut quaerat
-              exercitationem dolore error voluptates? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Quaerat, impedit eos. Voluptatibus
-              tempore voluptatum repellat eos quasi doloribus consequatur amet
-              possimus eaque rem. Explicabo, et quod vero dolor dolore voluptates.
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
-              error enim consequuntur eaque, quia excepturi beatae tempora
-              deserunt delectus, quibusdam hic maxime labore. In aut quaerat
-              exercitationem dolore error voluptates? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Quaerat, impedit eos. Voluptatibus
-              tempore voluptatum repellat eos quasi doloribus consequatur amet
-              possimus eaque rem. Explicabo, et quod vero dolor dolore voluptates.
+              possimus eaque rem. Explicabo, et quod vero dolor dolore
+              voluptates. Lorem ipsum, dolor sit amet consectetur adipisicing
+              elit. Expedita error enim consequuntur eaque, quia excepturi
+              beatae tempora deserunt delectus, quibusdam hic maxime labore. In
+              aut quaerat exercitationem dolore error voluptates? Lorem ipsum
+              dolor sit amet consectetur adipisicing elit. Quaerat, impedit eos.
+              Voluptatibus tempore voluptatum repellat eos quasi doloribus
+              consequatur amet possimus eaque rem. Explicabo, et quod vero dolor
+              dolore voluptates.
             </div>
           </div>
         </VuePerfectScrollbar>
@@ -185,17 +196,17 @@ export default {
   props: ["eventData"],
   data() {
     return {
-      activeColor: {name: 'orange', code: '#FFB94A'},
+      activeColor: { name: "orange", code: "#FFB94A" },
       colors: [
-        {name: 'blue', code: '#85CFFF'},
-        {name: 'violet', code: '#BE92E3'},
-        {name: 'green', code: '#7BD48A'},
-        {name: 'gray', code: '#999999'},
-        {name: 'orange', code: '#FFB94A'},
-        {name: 'red', code: '#FF8787'},
+        { name: "blue", code: "#85CFFF" },
+        { name: "violet", code: "#BE92E3" },
+        { name: "green", code: "#7BD48A" },
+        { name: "gray", code: "#999999" },
+        { name: "orange", code: "#FFB94A" },
+        { name: "red", code: "#FF8787" }
       ],
       isShowColorDropdown: false
-    }
+    };
   },
   methods: {
     closePopup() {
@@ -214,5 +225,5 @@ export default {
 
 <style lang="scss" scoped>
 @import "../style";
-@import "./style"
+@import "./style";
 </style>
