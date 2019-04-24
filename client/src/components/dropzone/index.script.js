9936ce2691c3d8707c039b1c7ee82c20c6f9229d
Le Khang <khangle0608@gmail.com>
2019-04-24 16:45:36 +0700

    update file before merge branch yendt v240419

---
+++ client/src/components/dropzone/index.script.js
@@ -0,0 +1,37 @@
+export default {
+  props: [ "status" ],
+  data() {
+    return {
+      file: ""
+    };
+  },
+  computed: {
+    fileAvatar() {
+      return this.$store.getters.fileAvatar;
+    },
+    statusZone: {
+      set( value ) {
+        this.$emit( "close", value );
+      },
+      get() {
+        return this.status;
+      }
+    }
+  },
+  methods: {
+    closeZone() {
+      this.$emit( "close", false );
+    },
+    selectFile() {
+      this.file = this.$refs.file.files[ 0 ];
+      this.sendFile();
+    },
+    sendFile() {
+      const formData = new FormData();
+
+      formData.append( "file", this.file );
+      this.$store.dispatch( "sendFile", formData );
+      this.closeZone();
+    }
+  }
+};
