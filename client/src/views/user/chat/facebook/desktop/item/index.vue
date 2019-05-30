ddd0241d80cb27b0cef1d4e36da7e666a820fa49
Le Khang <khanggle0608@gmail.com>
2019-05-30 18:42:49 +0700

    update file drop

---
+++ client/src/views/user/chat/facebook/desktop/item/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="card" v-if="item">
+    <div class="card_body">
+      <div class="card--header">
+        <delete-popup :content="item" />  
+      </div>
+      <div class="card--content">
+        <div class="avatar">
+          <img class="picture" :src="item.userInfo.thumbSrc" />
+          <span class="status" :class="item.status === true ? 'active' : ''">
+          </span>
+        </div>
+        <h3 class="name">{{ item.userInfo.name }}</h3>
+        <button class="btn btn--connect" v-if="item.status === true">
+          Đang hoạt động
+        </button>
+        <!-- if cookie dont use show button-->
+        <button
+          v-else
+          class="btn btn--update"
+          @click="isModalUpdateCookie = true"
+        >
+          Cập nhật
+        </button>
+      </div>
+      <div class="card--footer">
+        <div class="left">
+          <p>Kết nối lần cuối</p>
+          <p>{{ item.updated_at | covertDateUpdatedAt }}</p>
+        </div>
+      </div>
+    </div>
+    <transition name="popup">
+      <update-cookie
+        :item="item"
+        v-if="isModalUpdateCookie === true"
+        :popupData="isModalUpdateCookie"
+        @closeAddPopup="isModalUpdateCookie = $event"
+        :nameBread="nameUpdatePopup"
+        :subBread="descUpdatePopup"></update-cookie>
+    </transition>
+  </div>
+</template>
+<script>
+import DeletePopup from "@/components/shared/layouts/popupdelete/deleteAccountFb";
+export default {
+  components: {
+    DeletePopup
+  },
+  filters: {
+    covertDateUpdatedAt(d) {
+      const newDate = new Date(d);
+      const year = newDate.getFullYear();
+      const month = newDate.getMonth() + 1;
+      const date = newDate.getDate();
+      const hour = newDate.getHours();
+      let minutes = newDate.getMinutes();
+      if (minutes < 10) minutes = minutes + "0";
+      return `${hour}:${minutes}, ${date}/${month}/${year}`;
+    }
+  },
+  props: ["item"],
+  data() {
+    return {
+      isModalUpdateCookie: false,
+      nameUpdatePopup: "Cập nhật mã kích hoạt",
+      descUpdatePopup:
+        "Dán mã kích hoạt Facebook vào ô bên dưới để cập nhật lại mã kích hoạt tài khoản."
+    };
+  },
+  methods: {
+  }
+};
+</script>
+<style lang="scss" scoped>
+  @import "../index.style";
+</style>
---
+++ server/src/helpers/services/default.service.js
@@ -0,0 +1,74 @@
+/* eslint-disable one-var */
+const PostCategory = require( "../../models/post/PostCategory.model" );
+const Block = require( "../../models/chat/Block.model" );
+const GroupBlock = require( "../../models/chat/GroupBlock.model" );
+const BroadCast = require( "../../models/chat/Broadcast.model" );
+const Sequence = require( "../../models/chat/Sequence.model" );
+
+const dictionaries = require( "../../configs/dictionaries" );
+
+module.exports = {
+  "defaulSchema": async ( newUser ) => {
+    // Create default postcategory
+    const defaultPostCategory = await new PostCategory( {
+      "title": dictionaries.DEFAULT_POSTCATEGORY
+    } );
+
+    await defaultPostCategory.save();
+
+    // create group default when signup
+
+    const defaultGroup = await new GroupBlock();
+
+    defaultGroup.name = "Mặc Định";
+    defaultGroup._account = newUser._id;
+    await defaultGroup.save();
+
+    // create block welcome in default
+
+    const defaultBlock = await new Block();
+
+    defaultBlock.name = "Welcome";
+    defaultBlock._account = newUser._id;
+    defaultBlock._groupBlock = defaultGroup._id;
+    await defaultBlock.save();
+    defaultGroup.blocks.push( defaultBlock._id );
+    await defaultGroup.save();
+
+    // Create block default in broadcast type schedule, deliver
+    const defaultDel = await new BroadCast();
+
+    // deliver
+    defaultDel.typeBroadCast = "Tin nhắn gửi ngay";
+    defaultDel._account = newUser._id;
+    await defaultDel.save();
+    // schedue
+    const defaultSchedule = await new BroadCast();
+
+    defaultSchedule.typeBroadCast = "Thiết lập bộ hẹn";
+    defaultSchedule._account = newUser._id;
+    await defaultSchedule.save();
+    const date = new Date();
+
+    date.setHours( 12, 0, 0 );
+    date.setDate( date.getDate() + 1 );
+    defaultSchedule.blocks.push( {
+      "timeSetting": {
+        "dateMonth": `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+        "hour": `${date.getHours() }:0${ date.getMinutes()}`,
+        "repeat": {
+          "typeRepeat": "Không",
+          "valueRepeat": ""
+        }
+      }
+    } );
+    await defaultSchedule.save();
+
+    // Create default sequence
+    const newSeq = await new Sequence();
+
+    newSeq.name = "Chuỗi kịch bản 0";
+    newSeq._account = newUser._id;
+    await newSeq.save();
+  }
+};
