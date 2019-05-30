63468390d0b68931a688360a589d99d0e9d9250f
yendang <dangthiyen103@gmail.com>
2019-05-30 17:05:51 +0700

    Commit before merge dev-hocpc-api

---
+++ client/src/store/modules/chat/broadcast.store.js
@@ -0,0 +1,24 @@
+import BroadcastService from "@/services/modules/chat/broadcast.service";
+
+const state = {
+  allBroadcasts: []
+};
+const getters = {
+  allBroadcasts: ( state ) => state.allBroadcasts
+};
+const mutations = {
+  getAllBroadcasts: ( state, payload ) => {
+
+  }
+};
+const actions = {
+  getAllBroadcasts: {
+
+  }
+};
+export default {
+  state,
+  getters,
+  mutations,
+  actions
+};
