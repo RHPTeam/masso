c0c80496eaac488ed92dd3b094625ff56cd81b5e
Le Khang <khanggle0608@gmail.com>
2019-06-02 04:11:14 +0700

    update file overwritten before checkout dev-khanglt-vue

---
+++ server/src/helpers/utils/usecases/schedule.js
@@ -0,0 +1,16 @@
+// Default when init system schedule object null
+const schedule = {}, keyDefault = "rhp";
+
+module.exports = {
+  set ( key, value ) {
+    schedule[ keyDefault + key ] = value;
+  },
+
+  get ( key ) {
+    return schedule[ keyDefault + key ];
+  },
+
+  objectKeyExists ( key ) {
+    return schedule.hasOwnProperty( keyDefault + key );
+  }
+};
