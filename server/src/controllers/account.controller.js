06a0c5570d825ab51aaaed10ae20442bf88779b7
hoc-anms <phamvanhoc96@gmail.com>
2019-06-06 17:34:24 +0700

    update api backup post, category post when conver from older database

--- server/src/controllers/account.controller.js
+++ server/src/controllers/account.controller.js
@@ -21,7 +21,7 @@ module.exports = {
     const { body, file } = req,
       userInfo = await Account.findOne( { "_id": req.uid } );
 
-    let data, resUserSync;
+    let data, resUserSync, avatar;
 
     // Check update user info if user upload profile
     if ( file ) {
@@ -34,8 +34,8 @@ module.exports = {
         return res.status( 404 ).json( { "status": "error", "message": "Máy chủ bạn đang hoạt động có vấn đề! Vui lòng liên hệ với bộ phận CSKH." } );
       }
 
-      userInfo.imageAvatar = `${process.env.APP_URL}:${process.env.PORT_BASE}/${file.path}`;
-      await userInfo.save();
+      avatar = `${process.env.APP_URL}:${process.env.PORT_BASE}/${file.path}`;
+      await Account.findByIdAndUpdate( userInfo._id, { "$set": { "imageAvatar": avatar } }, { "new": true } ).select( "-password -__v" );
 
       return res.status( 201 ).json( jsonResponse( "success", userInfo ) );
     }
