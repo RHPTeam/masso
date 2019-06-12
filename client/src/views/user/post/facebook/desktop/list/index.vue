8c392adeab81a46d43da97f9a74fd968d4d1de2d
hoc-anms <phamvanhoc96@gmail.com>
2019-06-12 11:42:45 +0700

    commit before merge branch dev-khanglt-vue

---
+++ client/src/views/user/post/facebook/desktop/list/index.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="wrapper">
+    <div class="alert alert_warning text_left">
+      CHÚ Ý! Khi quyết định xóa một tài khoản Facebook khỏi hệ thống, tất cả dữ
+      liệu được thiết lập trước đó của tài khoản sẽ bị dừng hoặc xóa. Liên hệ
+      với bộ phận CSKH của Zinbee để giải đáp thắc mắc nếu có.
+    </div>
+    <div class="list r">
+      <!-- Start: Add Account Item -->
+      <div class="addItem c_md_6 c_lg_4 c_xl_3">
+        <div class="card" @click="showPopup" :class="disabledClass()">
+          <div class="card_body d_flex align_items_center justify_content_center">
+            <div class="item--content">
+              <icon-base
+                class="icon--add"
+                icon-name="plus"
+                width="60"
+                height="60"
+                viewBox="0 0 60 60"
+              >
+                <icon-plus/>
+              </icon-base>
+
+              <p>Kết nối với Facebook</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- End: Add Account Item -->
+      <!-- Start: Account Items Loop -->
+      <div class="item c_md_6 c_lg_4 c_xl_3"
+           v-for="(item, index) in accountsFB"
+           :key="index"
+      >
+        <div class="card" v-if="facebookStatus === 'loading' && index === accountsFB.length - 1">
+          <div class="card_body text_center d_flex align_items_center justify_content_center">
+            <loading-component/>
+          </div>
+        </div>
+        <item-account
+          v-else
+          :item="item"
+        />
+      </div>
+      <!-- End: Account Items Loop -->
+    </div>
+
+    <!-- ************** POPUP *************** -->
+    <transition name="popup">
+      <upgrade-pro-popup
+        v-if="showUpgradePro === true"
+        :data-theme="currentTheme"
+        :showUpgradePro="showUpgradePro"
+        @closeAddPopup="showUpgradePro = $event"
+      />
+      <add-account-popup
+        v-if="showModal === true"
+        @closeAddPopup="showModal = $event"
+      ></add-account-popup>
+      <existed-account-popup
+        v-if="this.$store.getters.addAccountError === 'error'"
+        :data-theme="currentTheme"
+      />
+    </transition>
+  </div>
+</template>
+
+<script>
+import AddAccountPopup from "../popup/list/addaccount/index";
+import ExistedAccountPopup from "../popup/list/alert/index";
+import UpgradeProPopup from "@/components/shared/layouts/upgradepro";
+import ItemAccount from "./item/index";
+
+export default {
+  components: {
+    AddAccountPopup,
+    ExistedAccountPopup,
+    UpgradeProPopup,
+    ItemAccount
+  },
+  data() {
+    return {
+      showModal: false,
+      showUpgradePro: false,
+      nameBread: "Thêm tài khoản Facebook",
+      subBread: "Dán mã kích hoạt Facebook vào ô bên dưới để thêm tài khoản."
+    };
+  },
+  computed: {
+    accountsFB() {
+      return this.$store.getters.accountsFB;
+    },
+    currentTheme() {
+      return this.$store.getters.themeName;
+    },
+    user() {
+      return this.$store.getters.userInfo;
+    },
+    facebookStatus() {
+      return this.$store.getters.facebookStatus;
+    }
+  },
+  async created() {
+    await this.$store.dispatch( "getAccountsFB" );
+  },
+  methods: {
+    showPopup() {
+      if (this.accountsFB.length >= this.user.maxAccountFb) {
+        this.showUpgradePro = true;
+      } else {
+        this.showModal = true;
+      }
+    },
+    disabledClass() {
+      if (this.accountsFB.length >= this.user.maxAccountFb) {
+        return {
+          disabled: true
+        };
+      } else {
+        return {
+          disabled: false
+        };
+      }
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "./index.style";
+</style>
---
+++ client/src/views/user/post/facebook/desktop/list/item/index.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="card" v-if="item">
+    <div class="card_body">
+      <!-- Start: Card Header -->
+      <div class="card--header d_flex align_items_center justify_content_between">
+        <div class="btn--setting" @click="viewDetail">
+          <icon-base
+          >
+            <icon-info
+            ></icon-info>
+          </icon-base>
+        </div>
+        <div class="btn--remove" @click="isDeleteItemAccount = true">
+          <icon-base
+            class="icon--remove"
+            icon-name="Xóa tài khoản"
+            width="20"
+            height="20"
+            viewBox="0 0 15 15">
+            <icon-remove />
+          </icon-base>
+        </div>
+      </div>
+      <!-- End: Card Header -->
+      <!-- Start: Card Content -->
+      <div class="card--content">
+        <div class="avatar"
+             v-if="item.userInfo.thumbSrc && item.userInfo.thumbSrc.length > 0"
+             @click=viewDetail
+        >
+          <img class="picture" :src="item.userInfo.thumbSrc" />
+          <span class="status" :class="item.status === true ? 'active' : ''">
+          </span>
+        </div>
+        <h3 class="name"
+            @click="viewDetail"
+        >{{ item.userInfo.name }}</h3>
+        <button class="btn btn--connect" v-if="item.status === true">
+          Đang hoạt động
+        </button>
+        <!-- if cookie dont use show button-->
+        <button
+          v-else
+          class="btn btn--update"
+          @click="isModalUpdateCookie = true"
+        >
+          Cập nhật
+        </button>
+      </div>
+      <!-- End: Card Content -->
+      <!-- Start: Card Footer -->
+      <div class="card--footer">
+        <div class="left">
+          <p>Kết nối lần cuối</p>
+          <p>{{ item.updated_at | covertDateUpdatedAt }}</p>
+        </div>
+      </div>
+      <!-- End: Card Footer-->
+    </div>
+    <transition name="popup">
+      <update-cookie
+        :item="item"
+        v-if="isModalUpdateCookie === true"
+        :popupData="isModalUpdateCookie"
+        @closeAddPopup="isModalUpdateCookie = $event"
+        :nameBread="nameUpdatePopup"
+        :subBread="descUpdatePopup"
+      >
+      </update-cookie>
+      <delete-item
+        v-if="isDeleteItemAccount === true"
+        title="Xoá tài khoản Facebook"
+        @closePopup="isDeleteItemAccount = $event"
+        storeActionName="deleteAccount"
+        :targetData="item"
+        typeName="tài khoản"
+      ></delete-item>
+    </transition>
+  </div>
+</template>
+<script>
+import UpdateCookie from "../../popup/list/updatecookie/index";
+import DeleteItem from "../../popup/list/delete/index";
+export default {
+  components: {
+    UpdateCookie,
+    DeleteItem
+  },
+  filters: {
+    covertDateUpdatedAt(d) {
+      const newDate = new Date(d);
+      const year = newDate.getFullYear();
+      const month = newDate.getMonth() + 1;
+      const date = newDate.getDate();
+      const hour = newDate.getHours();
+      let minutes = newDate.getMinutes();
+      if (minutes < 10) minutes = minutes + "0";
+      return `${hour}:${minutes}, ${date}/${month}/${year}`;
+    }
+  },
+  props: ["item"],
+  data() {
+    return {
+      isModalUpdateCookie: false,
+      isDeleteItemAccount: false,
+      nameUpdatePopup: "Cập nhật mã kích hoạt",
+      descUpdatePopup:
+        "Dán mã kích hoạt Facebook vào ô bên dưới để cập nhật lại mã kích hoạt tài khoản."
+    };
+  },
+  methods: {
+    viewDetail() {
+      this.$store.dispatch( "getFBAccountById", this.item._id );
+      this.$router.push( { name: "post_fbaccount_detail",
+                           params: { fbAccountId: this.item._id } } );
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+@import "../index.style";
+</style>
