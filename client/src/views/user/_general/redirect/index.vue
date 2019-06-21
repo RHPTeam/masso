367cb5c8ad2e35202cd8fdbb4929e15b5a334463
Sky Albert <skyalbert@Skys-MacBook-Pro.local>
2019-06-22 04:20:37 +0700

    Update file fix scss and redirect

--- client/src/views/user/_general/redirect/index.vue
+++ client/src/views/user/_general/redirect/index.vue
@@ -7,29 +7,31 @@
 import axios from "axios";
 import CookieFunction from "@/utils/functions/cookie";
 import StringFunction from "@/utils/functions/string";
+
+const checkRedirect = ( query ) => {
+  return new Promise( (resolve) => {
+    if ( query.hasOwnProperty( "authorization" ) ) {
+      CookieFunction.setCookie( "sid", StringFunction.findSubString( query.authorization, "sid=", ";" ) );
+      CookieFunction.setCookie( "uid", StringFunction.findSubString( query.authorization, "uid=", ";" ) );
+      CookieFunction.setCookie( "cfr", StringFunction.findSubString( query.authorization, "cfr=", ";" ) );
+      CookieFunction.setCookie( "token", "w,e#$oUFC99|_ADQ&,jv,9fd*Z4N<\"4!m.]s<3//Hy?oSTmZ.xj&'Y2..J47W+n" );
+      CookieFunction.setCookie( "__v", "1.0.4" );
+      axios.defaults.headers.common.Authorization = query.authorization;
+      if ( query.authorization && CookieFunction.getCookie( "sid" ).length > 0 && axios.defaults.headers.common.Authorization.length > 0 ) {
+        resolve( true );
+      }
+      resolve( false );
+    }
+  } );
+};
+
 export default {
   async created() {
-    const statusRedirect = await this.checkRedirect();
+    const query = this.$route.query;
+    const statusRedirect = await checkRedirect( query );
     if ( statusRedirect === true ) {
       this.$router.push( "/welcome" );
-    }
-  },
-  methods: {
-    async checkRedirect() {
-      const query = this.$route.query;
-      if ( query.hasOwnProperty( "authorization" ) ) {
-        CookieFunction.setCookie( "sid", StringFunction.findSubString( query.authorization, "sid=", ";" ) );
-        CookieFunction.setCookie( "uid", StringFunction.findSubString( query.authorization, "uid=", ";" ) );
-        CookieFunction.setCookie( "cfr", StringFunction.findSubString( query.authorization, "cfr=", ";" ) );
-        CookieFunction.setCookie( "token", "w,e#$oUFC99|_ADQ&,jv,9fd*Z4N<\"4!m.]s<3//Hy?oSTmZ.xj&'Y2..J47W+n" );
-        CookieFunction.setCookie( "__v", "1.0.4" );
-        if ( CookieFunction.getCookie( "sid" ).length > 0 ) {
-          axios.defaults.headers.common.Authorization = query.authorization;
-          return true;
-        }
-      } else {
-        return false;
-      }
+      this.$router.go(1);
     }
   }
 }
