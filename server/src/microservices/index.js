798f4d66a6a55ee0df03ec4cdd45a1fc400eab5b
Sky Albert <skyalbert@Skys-MacBook-Pro.local>
2019-06-21 21:03:42 +0700

    update file v24 - fix create facebook save token, logs campaign

--- server/src/microservices/index.js
+++ server/src/microservices/index.js
@@ -24,4 +24,5 @@ server.listen( process.env.PORT_SOCKET );
 
 io.on( "connection", async ( socket ) => {
   console.log( `Client from ${process.env.APP_URL}:${process.env.PORT_BASE} connected with id: ${socket.id}` );
+
 } );
