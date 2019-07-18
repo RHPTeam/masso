c62a6808f98dc848c90af974b1626ed37c5ae4c2
hoc-anms <phamvanhoc96@gmail.com>
2019-07-18 10:29:12 +0700

    restructure api attribute chat

--- server/src/controllers/chat/attribute.controller.js
+++ server/src/controllers/chat/attribute.controller.js
@@ -7,14 +7,12 @@
  * team: BE-RHP
  */
 /* eslint-disable one-var */
-const Account = require( "../../models/Account.model" );
 const Friend = require( "../../models/chat/Friend.model" );
 const Attribute = require( "../../models/chat/Attribute.model" );
+const Vocate = require( "../../models/chat/Vocate.model" );
 
 const jsonResponse = require( "../../configs/response" );
-const secure = require( "../../helpers/utils/secures/jwt" );
-const ArrayFunction = require( "../../helpers/utils/functions/array" );
-const { findSubString } = require( "../../helpers/utils/functions/string" );
+const { removeDuplicates } = require( "../../helpers/utils/functions/array" );
 
 module.exports = {
   /**
@@ -24,28 +22,46 @@ module.exports = {
    *	Note: Api define admin and members, if u're admin then u can get all data, on the contrary u just can get data of you created before
    */
   "index": async ( req, res ) => {
-    let dataResponse = null;
-    const authorization = req.headers.authorization,
-      role = findSubString( authorization, "cfr=", ";" ),
-      userId = secure( res, authorization ),
-      accountResult = await Account.findOne( { "_id": userId } );
+    let dataResponse = null, page;
 
-    if ( !accountResult ) {
-      return res.status( 403 ).json( jsonResponse( "Người dùng không tồn tại!", null ) );
+    // Object.entries( req.query ).length === 0 && req.query.constructor === Object
+    if ( req.query._id ) {
+      dataResponse = await Attribute.findOne( { "_id": req.query._id } ).lean();
+    } else if ( Object.entries( req.query ).length === 0 && req.query.constructor === Object ) {
+      dataResponse = await Attribute.find( {} ).lean();
     }
+    if ( req.query._id && req.query._friend === "true" && req.query._size && req.query._page ) {
+      const findAttribulte = await Attribute.findOne( { "_id": req.query._id } ).lean(),
+        dataFriend = await Promise.all( findAttribulte._friends.map( async ( friend ) => {
+          const findFriend = await Friend.findOne( { "userID": friend, "_account": req.uid }, "-_id -__v -created_at" ).lean(),
+            vocate = await Vocate.find( { "_account": req.uid, "_friends": friend } ).lean();
 
-    if ( role === "Member" ) {
-      !req.query ? dataResponse = await Attribute.find( { "_account": userId } ).populate( { "path": "_friends", "select": "-_account -_facebook" } ) : dataResponse = await Attribute.find( req.query ).populate( { "path": "_friends", "select": "-_account -_facebook" } );
-      if ( !dataResponse ) {
-        return res.status( 403 ).json( jsonResponse( "Thuộc tính không tồn tại" ) );
+          vocate.length === 0 ? ( findFriend.vocate = "Chưa thiết lập" ) : ( findFriend.vocate = vocate[ 0 ].name );
+          return findFriend;
+        } ) );
+
+      dataResponse = dataFriend.slice(
+        ( Number( req.query._page ) - 1 ) * Number( req.query._size ),
+        Number( req.query._size ) * Number( req.query._page )
+      );
+      if ( dataResponse.length % req.query._size === 0 ) {
+        page = Math.floor( dataResponse.length / req.query._size );
+      } else {
+        page = Math.floor( dataResponse.length / req.query._size ) + 1;
       }
-      dataResponse = dataResponse.map( ( item ) => {
-        if ( item._account.toString() === userId ) {
-          return item;
-        }
-      } ).filter( ( item ) => item !== undefined );
+
+      if ( !dataResponse ) {
+        return res.status( 403 ).json( jsonResponse( "fail", null ) );
+      }
+      return res.status( 200 ).json( jsonResponse( "success", { "results": dataResponse, "page": page } ) );
+
+
     }
-    res.status( 200 ).json( jsonResponse( "Lấy dữ liệu thành công =))", dataResponse ) );
+    if ( !dataResponse ) {
+      return res.status( 403 ).json( jsonResponse( "fail", null ) );
+    }
+
+    res.status( 200 ).json( jsonResponse( "success", dataResponse ) );
   },
   /**
    * What?
@@ -54,19 +70,13 @@ module.exports = {
    *
    */
   "create": async ( req, res ) => {
-    const userId = secure( res, req.headers.authorization ),
-      accountResult = await Account.findOne( { "_id": userId } );
-
-    if ( !accountResult ) {
-      res.status( 403 ).json( jsonResponse( "Người dùng không tồn tại!", null ) );
-    }
     const objectSaver = {
         "name": req.body.name,
         "value": req.body.value
       },
       attribute = await new Attribute( objectSaver );
 
-    attribute._account = userId;
+    attribute._account = req.uid;
     await attribute.save();
     res.status( 201 ).json( jsonResponse( "Tạo thuộc tính thành công =))", attribute ) );
   },
@@ -77,15 +87,9 @@ module.exports = {
    *
    */
   "update": async ( req, res ) => {
-    const userId = secure( res, req.headers.authorization ),
-      accountResult = await Account.findOne( { "_id": userId } );
-
-    if ( !accountResult ) {
-      res.status( 403 ).json( jsonResponse( "Người dùng không tồn tại!", null ) );
-    }
     const attrResult = await Attribute.findOne( { "_id": req.query._attrId } );
 
-    if ( attrResult._account.toString() !== userId ) {
+    if ( attrResult._account.toString() !== req.uid ) {
       return res.status( 405 ).json( jsonResponse( "Bạn không có quyền cho mục này!", null ) );
     }
     if ( !attrResult ) {
@@ -109,18 +113,12 @@ module.exports = {
    *
    */
   "delete": async ( req, res ) => {
-    const userId = secure( res, req.headers.authorization ),
-      accountResult = await Account.findOne( { "_id": userId } );
-
-    if ( !accountResult ) {
-      res.status( 403 ).json( jsonResponse( "Người dùng không tồn tại!", null ) );
-    }
     const attrResult = await Attribute.findOne( { "_id": req.query._attrId } );
 
     if ( !attrResult ) {
       res.status( 403 ).json( jsonResponse( "Thuộc tính này không tồn tại!", null ) );
     }
-    if ( attrResult._account.toString() !== userId ) {
+    if ( attrResult._account.toString() !== req.uid ) {
       return res.status( 405 ).json( jsonResponse( "Bạn không có quyền cho mục này!", null ) );
     }
     await attrResult.remove();
@@ -134,16 +132,10 @@ module.exports = {
    */
   "filter": async ( req, res ) => {
     let data = [];
-    const userId = secure( res, req.headers.authorization ),
-      accountResult = await Account.findOne( { "_id": userId } );
-
-    if ( !accountResult ) {
-      res.status( 403 ).json( jsonResponse( "Người dùng không tồn tại!", null ) );
-    }
 
     // Filter attribute by name
     if ( req.body.name && !req.body.value && !req.query._type ) {
-      const foundAttribute = await Attribute.find( { "_account": accountResult._id, "name": req.body.name } );
+      const foundAttribute = await Attribute.find( { "_account": req.uid, "name": req.body.name } );
 
       if ( foundAttribute.length < 0 ) {
         return res.status( 403 ).json( jsonResponse( "Không tìm thấy attribue", null ) );
@@ -153,15 +145,15 @@ module.exports = {
           data.push( friend.toString() );
         } );
       } ) );
-      data = ArrayFunction.removeDuplicates( data );
+      data = await removeDuplicates( data );
       Promise.all( data.map( async ( friend ) => {
-        const foundFriend = await Friend.findById( friend );
+        const foundFriend = await Friend.findOne( { "userID": friend } );
 
         return foundFriend;
       } ) ).then( async ( item ) => {
         // Get vocate of friends before return to client
         const friendsFixed = item.map( async ( friend ) => {
-            let vocate = await Vocate.find( { "_account": userId, "_friends": friend._id } );
+            let vocate = await Vocate.find( { "_account": req.uid, "_friends": friend.userID } );
 
             vocate.length === 0 ? friend.vocate = "Chưa thiết lập" : friend.vocate = vocate[ 0 ].name;
             return friend;
@@ -173,7 +165,7 @@ module.exports = {
     }
     // Filter attribute by name, value
     if ( req.body.name && req.body.value && req.query._type === "is" ) {
-      const foundAttribute = await Attribute.find( { "_account": accountResult._id, "name": req.body.name, "value": req.body.value } );
+      const foundAttribute = await Attribute.find( { "_account": req.uid, "name": req.body.name, "value": req.body.value } );
 
       if ( foundAttribute.length < 0 ) {
         return res.status( 403 ).json( jsonResponse( "Không tìm thấy attribue", null ) );
@@ -183,15 +175,15 @@ module.exports = {
           data.push( friend.toString() );
         } );
       } ) );
-      data = ArrayFunction.removeDuplicates( data );
+      data = await removeDuplicates( data );
       Promise.all( data.map( async ( friend ) => {
-        const foundFriend = await Friend.findById( friend );
+        const foundFriend = await Friend.findOne( { "userID": friend } );
 
         return foundFriend;
       } ) ).then( async ( item ) => {
         // Get vocate of friends before return to client
         const friendsFixed = item.map( async ( friend ) => {
-            let vocate = await Vocate.find( { "_account": userId, "_friends": friend._id } );
+            let vocate = await Vocate.find( { "_account": req.uid, "_friends": friend.userID } );
 
             vocate.length === 0 ? friend.vocate = "Chưa thiết lập" : friend.vocate = vocate[ 0 ].name;
             return friend;
@@ -203,7 +195,7 @@ module.exports = {
     }
     // Filter attribute by name, not value
     if ( req.body.name && req.body.value && req.query._type === "is_not" ) {
-      const foundAttribute = await Attribute.find( { "_account": accountResult._id, "name": req.body.name } );
+      const foundAttribute = await Attribute.find( { "_account": req.uid, "name": req.body.name } );
 
       if ( foundAttribute.length < 0 ) {
         return res.status( 403 ).json( jsonResponse( "Không tìm thấy attribue", null ) );
@@ -215,15 +207,15 @@ module.exports = {
           } );
         }
       } ) );
-      data = ArrayFunction.removeDuplicates( data );
+      data = removeDuplicates( data );
       Promise.all( data.map( async ( friend ) => {
-        const foundFriend = await Friend.findById( friend );
+        const foundFriend = await Friend.findOne( { "userID": friend } );
 
         return foundFriend;
       } ) ).then( async ( item ) => {
         // Get vocate of friends before return to client
         const friendsFixed = item.map( async ( friend ) => {
-            let vocate = await Vocate.find( { "_account": userId, "_friends": friend._id } );
+            let vocate = await Vocate.find( { "_account": req.uid, "_friends": friend.userID } );
 
             vocate.length === 0 ? friend.vocate = "Chưa thiết lập" : friend.vocate = vocate[ 0 ].name;
             return friend;
