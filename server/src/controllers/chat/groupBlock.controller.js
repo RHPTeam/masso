223445b1f873da35e216dae2e97bac7dd60bf71c
hoc-anms <phamvanhoc96@gmail.com>
2019-07-19 14:45:24 +0700

    update api block, group block restructure

--- server/src/controllers/chat/groupBlock.controller.js
+++ server/src/controllers/chat/groupBlock.controller.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-expressions */
 /* eslint-disable one-var */
 /**
  * Controller item block for project
@@ -8,16 +7,13 @@
  * team: BE-RHP
  */
 
-const Account = require( "../../models/Account.model" );
 const Block = require( "../../models/chat/Block.model" );
 const GroupBlock = require( "../../models/chat/GroupBlock.model" );
 const Sequence = require( "../../models/chat/Sequence.model" );
 
 const jsonResponse = require( "../../configs/response" );
-const secure = require( "../../helpers/utils/secures/jwt" );
 const convertUnicode = require( "../../helpers/utils/functions/unicode" );
 const Dictionaries = require( "../../configs/dictionaries" );
-const { findSubString } = require( "../../helpers/utils/functions/string" );
 
 module.exports = {
   /**
@@ -28,28 +24,14 @@ module.exports = {
    */
   "index": async ( req, res ) => {
     let dataResponse = null;
-    const authorization = req.headers.authorization;
-    const role = findSubString( authorization, "cfr=", ";" );
 
-    const userId = secure( res, authorization );
-    const accountResult = await Account.findOne( { "_id": userId } );
-
-    if ( !accountResult ) {
-      return res.status( 403 ).json( jsonResponse( "Người dùng không tồn tại!", null ) );
+    if ( req.query._id ) {
+      dataResponse = await GroupBlock.find( { "_id": req.query._id, "_account": req.uid } ).populate( { "path": "blocks", "select": "name" } ).lean();
+    } else if ( Object.entries( req.query ).length === 0 && req.query.constructor === Object ) {
+      dataResponse = await GroupBlock.find( { "_account": req.uid } ).populate( { "path": "blocks", "select": "name" } ).lean();
     }
 
-    if ( role === "Member" ) {
-      !req.query._id ? dataResponse = await GroupBlock.find( { "_account": userId } ).populate( { "path": "blocks", "select": "name" } ) : dataResponse = await GroupBlock.find( { "_id": req.query._id, "_account": userId } ).populate( { "path": "blocks", "select": "name" } );
-      if ( !dataResponse ) {
-        return res.status( 403 ).json( jsonResponse( "Thuộc tính không tồn tại" ) );
-      }
-      dataResponse = dataResponse.map( ( item ) => {
-        if ( item._account.toString() === userId ) {
-          return item;
-        }
-      } );
-    }
-    res.status( 200 ).json( jsonResponse( "Lấy dữ liệu thành công =))", dataResponse ) );
+    res.status( 200 ).json( jsonResponse( "success", dataResponse ) );
   },
   /**
    *  create block by user
@@ -58,13 +40,7 @@ module.exports = {
    *
    */
   "create": async ( req, res ) => {
-    const userId = secure( res, req.headers.authorization ),
-      foundUser = await Account.findOne( { "_id": userId } ).select( "-password" );
-
-    if ( !foundUser ) {
-      return res.status( 403 ).json( jsonResponse( "Người dùng không tồn tại!", null ) );
-    }
-    const foundGroupBlock = await GroupBlock.find( { "_account": userId } );
+    const foundGroupBlock = await GroupBlock.find( { "_account": req.uid } ).lean();
 
     // handle num for name
     let nameArr = foundGroupBlock.map( ( groupBlock ) => {
@@ -77,14 +53,14 @@ module.exports = {
       }
       return true;
     } ).map( ( item ) => parseInt( item.slice( Dictionaries.GROUPBLOCK.length ) ) );
-    const indexCurrent = Math.max( ...nameArr );
+    const indexCurrent = Math.max( ...nameArr ),
 
-    const newGroupBlock = await new GroupBlock();
+      newGroupBlock = await new GroupBlock();
 
     newGroupBlock.name = indexCurrent.toString() === "NaN" || foundGroupBlock.length === 0 || nameArr.length === 0 ? `${Dictionaries.GROUPBLOCK} 0` : `${Dictionaries.GROUPBLOCK} ${indexCurrent + 1}`;
-    newGroupBlock._account = userId;
+    newGroupBlock._account = req.uid;
     await newGroupBlock.save();
-    res.status( 200 ).json( jsonResponse( "Tạo nhóm block thành công!", newGroupBlock ) );
+    res.status( 200 ).json( jsonResponse( "success", newGroupBlock ) );
   },
   /**
    *  Add block by user
@@ -93,40 +69,34 @@ module.exports = {
    *
    */
   "addBlock": async ( req, res ) => {
-    const userId = secure( res, req.headers.authorization ),
-      foundUser = await Account.findOne( { "_id": userId } ).select( "-password" );
-
-    if ( !foundUser ) {
-      return res.status( 403 ).json( jsonResponse( "Người dùng không tồn tại!", null ) );
-    }
-    const foundGroupBlock = await GroupBlock.findOne( { "_id": req.query._groupId, "_account": userId } );
+    const foundGroupBlock = await GroupBlock.findOne( { "_id": req.query._groupId, "_account": req.uid } );
 
     if ( !foundGroupBlock ) {
-      return res.status( 403 ).json( jsonResponse( "Nhóm block không tồn tại!", null ) );
+      return res.status( 403 ).json( jsonResponse( "fail", "Nhóm block không tồn tại!" ) );
     }
 
     // Push block to item block and pull in item block before
     if ( req.query._blockId ) {
-      const foundBlock = await Block.findOne( { "_id": req.query._blockId, "_account": userId } );
+      const foundBlock = await Block.findOne( { "_id": req.query._blockId, "_account": req.uid } );
 
       if ( !foundBlock ) {
-        return res.status( 403 ).json( jsonResponse( "Bạn không có kịch bản này!", null ) );
+        return res.status( 403 ).json( jsonResponse( "fail", "Bạn không có kịch bản này!" ) );
       }
       if ( foundBlock._groupBlock === undefined ) {
-        return res.status( 405 ).json( jsonResponse( "Có lỗi xảy ra, vui lòng kiểm tra lại kịch bản muốn thêm!", null ) );
+        return res.status( 405 ).json( jsonResponse( "fail", "Có lỗi xảy ra, vui lòng kiểm tra lại kịch bản muốn thêm!" ) );
       }
-      const foundGroup = await GroupBlock.findOne( { "_id": foundBlock._groupBlock, "_account": userId } );
+      const foundGroup = await GroupBlock.findOne( { "_id": foundBlock._groupBlock, "_account": req.uid } ),
 
-      // Check block is already this item block
-      const isInArray = foundGroupBlock.blocks.some( ( id ) => {
-        return id.equals( req.query._blockId );
-      } );
+        // Check block is already this item block
+        isInArray = foundGroupBlock.blocks.some( ( id ) => {
+          return id.equals( req.query._blockId );
+        } );
 
       if ( isInArray ) {
-        return res.status( 403 ).json( jsonResponse( "Bạn đã thêm kịch bản vào nhóm kịch bản này!", null ) );
+        return res.status( 403 ).json( jsonResponse( "fail", "Bạn đã thêm kịch bản vào nhóm kịch bản này!" ) );
       }
       if ( foundGroup.name === "Chuỗi Kịch Bản" ) {
-        const foundSequence = await Sequence.find( { "_account": userId } );
+        const foundSequence = await Sequence.find( { "_account": req.uid } );
         let result = null;
 
         await Promise.all( foundSequence.map( ( val ) => {
@@ -153,7 +123,7 @@ module.exports = {
         await foundGroup.save();
         foundGroupBlock.blocks.push( req.query._blockId );
         await foundGroupBlock.save();
-        return res.status( 200 ).json( jsonResponse( "Thêm kịch bản từ trình tự kịch bản vào nhóm kịch bản thành công!", {
+        return res.status( 200 ).json( jsonResponse( "success", {
           "groupBlock": foundGroupBlock,
           "sequence": result,
           "block": foundBlock
@@ -165,7 +135,7 @@ module.exports = {
       await foundGroup.save();
       foundGroupBlock.blocks.push( req.query._blockId );
       await foundGroupBlock.save();
-      return res.status( 200 ).json( jsonResponse( "Thêm kịch bản từ nhóm kịch bản khác vào nhóm kịch bản này thành công!", {
+      return res.status( 200 ).json( jsonResponse( "success", {
         "groupBlock": foundGroupBlock,
         "block": foundBlock
       } ) );
@@ -184,12 +154,12 @@ module.exports = {
     const newBlock = new Block();
 
     newBlock.name = `Kịch Bản ${num}`;
-    newBlock._account = userId;
+    newBlock._account = req.uid;
     newBlock._groupBlock = req.query._groupId;
     await newBlock.save();
     foundGroupBlock.blocks.push( newBlock._id );
     await foundGroupBlock.save();
-    res.status( 200 ).json( jsonResponse( "Thêm block trong nhóm block thành công!", foundGroupBlock ) );
+    res.status( 200 ).json( jsonResponse( "success", foundGroupBlock ) );
   },
   /**
    *  update item block by user
@@ -198,21 +168,8 @@ module.exports = {
    *
    */
   "update": async ( req, res ) => {
-    const userId = secure( res, req.headers.authorization ),
-      foundUser = await Account.findOne( { "_id": userId } ).select( "-password" );
-    const {
-      body,
-      query
-    } = req;
-
-    if ( !foundUser ) {
-      return res.status( 403 ).json( jsonResponse( "Người dùng không tồn tại!", null ) );
-    }
-    if ( JSON.stringify( userId ) !== JSON.stringify( foundUser._id ) ) {
-      return res.status( 403 ).json( jsonResponse( "Lỗi truy cập!", null ) );
-    }
-    const dataGroupGroupUpdated = await GroupBlock.findOne( { "_id": query._groupId, "_account": userId } );
-    const findAllGroup = await GroupBlock.find( { "_account": userId } );
+    const dataGroupGroupUpdated = await GroupBlock.findOne( { "_id": query._groupId, "_account": req.uid } ),
+      findAllGroup = await GroupBlock.find( { "_account": req.uid } ).lean();
     // check name item block exists
     let checkName = false;
 
@@ -225,11 +182,11 @@ module.exports = {
       }
     } );
     if ( checkName ) {
-      return res.status( 403 ).json( jsonResponse( "Tên item đã tồn tại!", null ) );
+      return res.status( 403 ).json( jsonResponse( "fail", "Tên item đã tồn tại!" ) );
     }
     dataGroupGroupUpdated.name = body.name;
     await dataGroupGroupUpdated.save();
-    res.status( 201 ).json( jsonResponse( "Cập nhật nhóm block thành công!", dataGroupGroupUpdated ) );
+    res.status( 201 ).json( jsonResponse( "success", dataGroupGroupUpdated ) );
   },
   /**
    *  delete item block by user
@@ -238,19 +195,13 @@ module.exports = {
    *
    */
   "delete": async ( req, res, next ) => {
-    const userId = secure( res, req.headers.authorization ),
-      foundUser = await Account.findOne( { "_id": userId } ).select( "-password" );
-
-    if ( !foundUser ) {
-      return res.status( 403 ).json( jsonResponse( "Người dùng không tồn tại!", null ) );
-    }
-    const foundGroupBlock = await GroupBlock.findById( req.query._groupId );
+    const foundGroupBlock = await GroupBlock.findOne( { "_id": req.query._groupId, "_account": req.uid } );
 
     if ( !foundGroupBlock ) {
-      return res.status( 404 ).json( jsonResponse( "Nhóm block không tồn tại!", null ) );
+      return res.status( 404 ).json( jsonResponse( "fail", "Nhóm block không tồn tại!" ) );
     }
     // Delete block in item block
-    const foundDefaultGroup = await GroupBlock.findOne( { "name": "Mặc Định", "_account": userId } );
+    const foundDefaultGroup = await GroupBlock.findOne( { "name": "Mặc Định", "_account": req.uid } );
 
     if ( req.query._blockId ) {
       if ( foundGroupBlock.blocks.includes( req.query._blockId ) ) {
@@ -258,7 +209,7 @@ module.exports = {
         await foundGroupBlock.save();
         foundDefaultGroup.push( req.query._blockId );
         await foundDefaultGroup.save();
-        return res.status( 200 ).json( jsonResponse( "Xóa block trong nhóm block thành công! ", foundGroupBlock ) );
+        return res.status( 200 ).json( jsonResponse( "success", foundGroupBlock ) );
       }
       next();
     }
@@ -267,6 +218,6 @@ module.exports = {
       await foundDefaultGroup.save();
     } );
     await GroupBlock.findByIdAndRemove( req.query._groupId );
-    res.status( 200 ).json( jsonResponse( "Xóa nhóm block thành công!", null ) );
+    res.status( 200 ).json( jsonResponse( "success", null ) );
   }
 };
