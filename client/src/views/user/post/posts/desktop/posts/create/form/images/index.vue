d10cf71b85391baf132cf02c25de640cd53f1f76
Le Khang <khanggle0608@gmail.com>
2019-08-03 09:47:09 +0700

    update file before merge new branhc toantr

--- client/src/views/user/post/posts/desktop/posts/create/form/images/index.vue
+++ client/src/views/user/post/posts/desktop/posts/create/form/images/index.vue
@@ -86,6 +86,7 @@ export default {
 
       await this.$store.dispatch( "uploadPostAttachments", formData );
       const uploadFiles = this.postAttachmentsUpload.map( ( item ) => {
+        console.log(item);
         return {
           link: item,
           typeAttachment: 1
